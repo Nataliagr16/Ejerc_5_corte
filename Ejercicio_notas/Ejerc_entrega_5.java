a5004527efc5d683abe142cabbe3393b48b424ef
Nataliagr16 <112214790+Nataliagr16@users.noreply.github.com>
2022-11-19 23:40:27 -0500

    Add files via upload

---
+++ Ejercicio_notas/Ejerc_entrega_5.java
@@ -0,0 +1,9 @@
+
+public class Ejerc_entrega_5 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal;
+        miVentanaPrincipal = new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);        
+    }
+}
---
+++ Ejercicio_notas/Ejerc_notas.java
@@ -0,0 +1,48 @@
+ class Ejerc_notas {
+    double[] listaNotas;
+     
+     public Ejerc_notas(){
+         listaNotas = new double[5];
+     }
+    double calcularPromedio(){
+        double suma = 0;
+        for (int i=1; i < listaNotas.length ; i++){
+            suma = suma + listaNotas[i];
+        }
+        return (suma/listaNotas.length);
+    }
+    
+    double calcularDesviacion(){
+        double prom = calcularPromedio();
+        double suma = 0;
+        for (int i=0; i< listaNotas.length; i++){
+            suma += Math.pow(listaNotas[i] - prom,2);
+        }
+        return Math.sqrt(suma/listaNotas.length);
+    }
+
+    double calcularMayor() {
+        double mayor = listaNotas[0];
+        for (int i = 0; i < listaNotas.length; i++){
+            if (listaNotas[i] > mayor){
+                mayor = listaNotas[i];
+            }
+        }
+        return mayor;
+        
+    }
+
+    double calcularMenor() {
+        double menor = listaNotas[0];
+        for(int i=0; i <listaNotas.length; i++){
+            if (listaNotas[i] < menor){
+                menor = listaNotas[i];
+            }
+        }
+        return menor;
+    }
+    
+    
+
+    
+}
